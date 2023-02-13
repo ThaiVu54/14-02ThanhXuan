653069db7617d0372c282c6f6ff3d4e7399c63be
ThaiVu54 <88522037+ThaiVu54@users.noreply.github.com>
2023-02-13 23:17:34 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 const nameGirl = 'Em';
 const giftUrl = 'http://nodemy.vn';
 const eventName = 'Mừng Valentine 14/2';
-const titleCard = 'Tặng EM CHÁU ngày valentinô';
+const titleCard = 'Tặng EM ngày valentinô';
 
-const contentCard = 'Chúc em cháu của anh 14/02 tràn ngập niềm vui và những nụ cười. Mong điều đẹp nhất sẽ đến với em trong hôm nay và cả những ngày sau. ' +
+const contentCard = 'Chúc Người Yêu của anh 14/02 tràn ngập niềm vui và những nụ cười. Mong điều đẹp nhất sẽ đến với em trong hôm nay và cả những ngày sau. ' +
     'Nay là valentine đầu tiên chúng ta có nhau, anh không dám chắc tương lai sau này thế nào, nhưng anh ước chúng ta mãi luôn có nhau. Hãy cùng nhau cố gắng em nhé ❤🧡💙💜<br>' + 'TV 💘 TX'
 
 
